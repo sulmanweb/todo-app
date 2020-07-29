c9d1ddb59ba4b3af144e837bffdda15e257e48aa
Sulman Baig <sulmanweb@gmail.com>
2020-07-29 20:40:00 +0500

    Changing graphql playground from graphiql

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  mount GraphqlPlayground::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
