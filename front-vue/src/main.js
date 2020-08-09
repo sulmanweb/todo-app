829a527a6f5e0f054cddd02f7029dce352b06c25
Sulman Baig <sulmanweb@gmail.com>
2020-08-09 17:45:53 +0500

    Init vueJS App with sign In and Sign Up mutations on pages

---
+++ front-vue/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { createProvider } from "./vue-apollo";
+
+import "./assets/styles/index.css";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  apolloProvider: createProvider(),
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ front-vue/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+  },
+  {
+    path: "/sign_up",
+    name: "SignUp",
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/auth/signUp.vue"),
+  },
+  {
+    path: "/sign_in",
+    name: "SignIn",
+    component: () =>
+      import(/* webpackChunkName: "signin" */ "../views/auth/signIn.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
